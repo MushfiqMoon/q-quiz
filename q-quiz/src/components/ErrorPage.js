d6d8d933b2ac5b249cc3352539d54453dbf6bcaf
MushfiqMoon <mushfiq.moon013@google.com>
2022-10-11 01:54:22 +0600

    error page desing done

--- q-quiz/src/components/ErrorPage.js
+++ q-quiz/src/components/ErrorPage.js
@@ -1,22 +1,24 @@
 import React from 'react'
 import { useRouteError } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFaceFrownOpen } from '@fortawesome/free-regular-svg-icons'
 
 const ErrorPage = () => {
     const error = useRouteError()
     console.log(error)
-  return (
-    <div>
-        {
-            error && (
-                <div>
-                    <div>{error.status}</div>
-                    <div>{error.statusText || error.message}</div>
-                </div>
-            )
-        }
-        <p></p>
-    </div>
-  )
+    return (
+        <div className='vh-100 d-flex justify-content-center align-items-center flex-column'>
+            <FontAwesomeIcon icon={faFaceFrownOpen} style={{ fontSize: "200px", color: "#00b5ca" }} />
+            {
+                error && (
+                    <div className='text-center pb-5'>
+                        <div className='fs-1 text-danger'>{error.status}</div>
+                        <div className='fs-3'>{error.statusText || error.message}</div>
+                    </div>
+                )
+            }
+        </div>
+    )
 }
 
 export default ErrorPage
--- q-quiz/src/components/Hero.js
+++ q-quiz/src/components/Hero.js
@@ -4,6 +4,8 @@ import exams from "./exams.json";
 import {
     Container,
 } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFaceFrown } from '@fortawesome/free-regular-svg-icons'
 
 const Hero = () => {
     return (
@@ -13,6 +15,7 @@ const Hero = () => {
                     <h1 className='t-primary '>Q-Quiz works. <br />
                         CVs don’t.</h1>
                     <h3>Multi-Tasking Multistep Form Job Application. </h3>
+                    <FontAwesomeIcon icon={faFaceFrown} />
                     <p>Our screening tests identify the best candidates and make your hiring decisions faster, easier, and bias-free.</p>
                 </div>
                 <div className='d-md-w-50'><Lottie animationData={exams} loop={true} /></div>
