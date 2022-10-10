89422f28632aafaebad48a7d27b2daf11079c564
MushfiqMoon <mushfiq.moon013@google.com>
2022-10-10 23:55:30 +0600

    navbar added

--- q-quiz/src/components/Header.js
+++ q-quiz/src/components/Header.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import {
+    Container,
+    Nav,
+    Navbar,
+} from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
+    return (
+        <div className='q-header'>
+            <Navbar bg="dark" variant="dark" expand="lg">
+                <Container>
+                    <Navbar.Brand href="#home">
+                        <img src="./logo.png" width="50" alt="" srcset="" /> QUIZ
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ms-auto">
+                            <NavLink className='menu' to='/'>Home</NavLink>
+                            <NavLink className='menu' to='/page'>Page</NavLink>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div >
+    )
 }
 
 export default Header
