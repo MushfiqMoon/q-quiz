534f4dceb72efb15b6fe8094c591935bcfcbd71b
MushfiqMoon <mushfiq.moon013@google.com>
2022-10-11 07:17:54 +0600

    correct ans added on toast

--- q-quiz/src/components/QuizItem.js
+++ q-quiz/src/components/QuizItem.js
@@ -1,37 +1,59 @@
 import React from 'react'
+import {
+    Row,
+    Col,
+} from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEye } from '@fortawesome/free-regular-svg-icons'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const QuizItem = ({ data }) => {
 
     const { correctAnswer, id, options, question } = data;
+
+    const notify = () => toast.success(correctAnswer, {
+        position: "bottom-center",
+        autoClose: 1000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+    });
+
     console.log(data)
     return (
         <div className='mb-3'>
             <article>
-
-                <div className='questions'
-                    dangerouslySetInnerHTML={{ __html: question }}
-                />
-
+                <Row>
+                    <Col xs={10}>
+                        <div className='questions'
+                            dangerouslySetInnerHTML={{ __html: question }}
+                        />
+                    </Col>
+                    <Col xs={2}>
+                        <FontAwesomeIcon className='q-hint' icon={faEye} onClick={notify} />
+                    </Col>
+                </Row>
             </article>
 
             <form className="donation-form" action="">
-                <div className="row">
-
+                <Row>
                     {
                         options.map((option, index) =>
-                            <div className=" col-md-6" key={index}>
+                            <Col md={6} key={index}>
                                 <div className="radio-field">
                                     <input className="op-1" type="radio" name="donate-cause" />
                                     <label htmlFor="donate-cause">{option}</label>
                                 </div>
-                            </div>
+                            </Col>
                         )
                     }
-
-
-
-                </div>
+                </Row>
             </form>
+            <ToastContainer/>
 
         </div>
     )
